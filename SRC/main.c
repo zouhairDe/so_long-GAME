df2d62d4618843f2c235836378a9b5220d9b7e0f
Zouhair <121346419+zouhairDe@users.noreply.github.com>
2024-02-08 18:30:03 +0100

    Update main.c

--- SRC/main.c
+++ SRC/main.c
@@ -6,11 +6,25 @@
 /*   By: zouddach <zouddach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 22:40:23 by zouddach          #+#    #+#             */
-/*   Updated: 2024/02/08 18:10:39 by zouddach         ###   ########.fr       */
+/*   Updated: 2024/02/08 18:28:06 by zouddach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+void	free_mlx_imgs(t_data *data)
+{
+	if (data->imgs.coin || data->imgs.player || data->imgs.opened_door
+		|| data->imgs.closed_door || data->imgs.land || data->imgs.border)
+	{
+		mlx_destroy_image(data->mlx, data->imgs.border);
+		mlx_destroy_image(data->mlx, data->imgs.land);
+		mlx_destroy_image(data->mlx, data->imgs.closed_door);
+		mlx_destroy_image(data->mlx, data->imgs.opened_door);
+		mlx_destroy_image(data->mlx, data->imgs.player);
+		mlx_destroy_image(data->mlx, data->imgs.coin);
+	}
+}
 
 int	quite(t_data *data)
 {
